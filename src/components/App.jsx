5e711e01b6ed769403ba3f3ee5d036d11d72d35e
annared <annared1982@gmail.com>
2023-02-28 14:11:53 +0200

    fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,41 @@
-export const App = () => {
-  return (
-    <div>
-     
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+
+const API_KEY = '32381232-0d08b52c11723d23aba771294';
+
+// axios.defaults.baseURL = `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+export class App extends Component {
+  state = {
+    images: [],
+  };
+
+  // imageList = ({ images }) => {
+  //   const { id, webformatURL, largeImageURL } = this.state;
+  //   return {};
+  // };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    this.setState({ images: response.data.hits });
+    console.log(this.state.images);
+  }
+
+  render() {
+    const { images } = this.state;
+    return (
+      <div>
+        
+        <ul>
+          {images.map(({ id, webformatURL, largeImageURL }) => (
+            <li key={id}>
+              <img src={webformatURL} alt="" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
