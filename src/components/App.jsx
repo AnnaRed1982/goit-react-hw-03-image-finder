4b4b4aca05e813ff7de89264d959c916324454bd
annared <annared1982@gmail.com>
2023-02-28 21:22:14 +0200

    comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,26 @@
 import { Component } from 'react';
-import axios from 'axios';
 
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 
-const API_KEY = '32381232-0d08b52c11723d23aba771294';
-
-// axios.defaults.baseURL = `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-
 export class App extends Component {
   state = {
-    images: [],
     serchRequest: '',
   };
 
   handleFormSubmit = serchRequest => {
-    console.log(this.state.serchRequest);
     this.setState({ serchRequest });
   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevState.serchRequest !== this.state.serchRequest) {
-      const response = await axios.get(
-        `https://pixabay.com/api/?q=${this.state.serchRequest}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=1`
-      );
-      this.setState({ images: response.data.hits });
-      console.log(this.state.images);
-    }
-  }
-
   render() {
-    const { images } = this.state;
+    const { serchRequest } = this.state;
 
     return (
       <div className="app">
         <Searchbar onSubmit={this.handleFormSubmit} />
 
-        <ImageGallery images={images} />
+        <ImageGallery serchRequest={serchRequest} />
       </div>
     );
   }
 }
-
-
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,12 +1,43 @@
+import { Component } from 'react';
+import axios from 'axios';
+
 import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 
-export const ImageGallery = ({ images }) => {
-  return (
-    <ul className={css.ImageGallery}>
-      {images.map(({ id, webformatURL, tags, largeImageURL }) => (
-        <ImageGalleryItem key={id} webformatURL={webformatURL} tags={tags} />
-      ))}
-    </ul>
-  );
-};
+const API_KEY = '32381232-0d08b52c11723d23aba771294';
+
+export class ImageGallery extends Component {
+  state = {
+    images: [],
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevProps.serchRequest !== this.props.serchRequest) {
+      const response = await axios.get(
+        `https://pixabay.com/api/?q=${this.props.serchRequest}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=1`
+      );
+      
+      this.setState({ images: response.data.hits });
+    }
+  }
+
+  render() {
+    return (
+      <ul className={css.ImageGallery}>
+        {this.state.images.map(({ id, webformatURL, tags, largeImageURL }) => (
+          <ImageGalleryItem key={id} webformatURL={webformatURL} tags={tags} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+// export const ImageGallery = ({ images }) => {
+//   return (
+//     <ul className={css.ImageGallery}>
+//       {images.map(({ id, webformatURL, tags, largeImageURL }) => (
+//         <ImageGalleryItem key={id} webformatURL={webformatURL} tags={tags} />
+//       ))}
+//     </ul>
+//   );
+// };
