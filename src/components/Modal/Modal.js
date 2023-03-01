6bb47f30a3da50c6de44a8b126651be59450b9ff
annared <annared1982@gmail.com>
2023-03-01 18:24:59 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,12 +3,20 @@ import css from '../Modal/Modal.module.css';
 
 export class Modal extends Component {
   componentDidMount() {
-    window.addEventListener('keydown', e => {
-      if (e.code === 'Escape') {
-        this.props.onClose();
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      console.log('esc');
+      this.props.onClose();
+    }
+  };
+
   render() {
     const { largeImageURL } = this.props;
 
