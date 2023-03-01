632316d5a2215c6065832b5ef94533a09d4ca759
annared <annared1982@gmail.com>
2023-03-01 13:34:31 +0200

    spinner

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import axios from 'axios';
+import { Audio } from 'react-loader-spinner';
 
 import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
@@ -9,26 +10,57 @@ const API_KEY = '32381232-0d08b52c11723d23aba771294';
 export class ImageGallery extends Component {
   state = {
     images: [],
+    error: null,
+    status: null,
   };
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevProps.serchRequest !== this.props.serchRequest) {
-      const response = await axios.get(
-        `https://pixabay.com/api/?q=${this.props.serchRequest}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=1`
-      );
-      
-      this.setState({ images: response.data.hits });
+      this.setState({ status: 'pending' });
+      try {
+        const response = await axios.get(
+          `https://pixabay.com/api/?q=${this.props.serchRequest}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=1`
+        );
+
+        this.setState({ images: response.data.hits, status: 'resolved' });
+      } catch (error) {
+        this.setState({ status: 'rejected', error });
+      }
     }
   }
 
   render() {
-    return (
-      <ul className={css.ImageGallery}>
-        {this.state.images.map(({ id, webformatURL, tags, largeImageURL }) => (
-          <ImageGalleryItem key={id} webformatURL={webformatURL} tags={tags} />
-        ))}
-      </ul>
-    );
+    const { status, error, images } = this.state;
+
+    if (status === 'pending') {
+      return (
+        <Audio
+          height="580"
+          width="280"
+          radius="9"
+          color="blue"
+          ariaLabel="loading"
+        />
+      );
+    }
+
+    if (status === 'rejected') {
+      return <h2>Whoops, something went wrong: {error.message}</h2>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <ul className={css.ImageGallery}>
+          {images.map(({ id, webformatURL, tags, largeImageURL }) => (
+            <ImageGalleryItem
+              key={id}
+              webformatURL={webformatURL}
+              tags={tags}
+            />
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
@@ -41,3 +73,61 @@ export class ImageGallery extends Component {
 //     </ul>
 //   );
 // };
+
+// export class ImageGallery extends Component {
+//   state = {
+//     images: [],
+//     isLoading: false,
+//     error: null,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     if (prevProps.serchRequest !== this.props.serchRequest) {
+//       this.setState({ isLoading: true });
+//       try {
+//         const response = await axios.get(
+//           `https://pixabay.com/api/?q=${this.props.serchRequest}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=1`
+//         );
+
+//         this.setState({ images: response.data.hits });
+//       } catch (error) {
+//         this.setState({ error });
+//         console.log(error);
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
+
+//       // this.setState({ isLoading: true });
+//       // const response = await axios.get(
+//       //   `https://pixabay.com/api/?q=${this.props.serchRequest}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&page=1`
+//       // );
+
+//       // this.setState({ images: response.data.hits, isLoading: false });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         {this.state.error && (
+//           <p>Whoops, something went wrong: {this.state.error.message}</p>
+//         )}
+//         {this.state.isLoading ? (
+//           <h2>Loading</h2>
+//         ) : (
+//           <ul className={css.ImageGallery}>
+//             {this.state.images.map(
+//               ({ id, webformatURL, tags, largeImageURL }) => (
+//                 <ImageGalleryItem
+//                   key={id}
+//                   webformatURL={webformatURL}
+//                   tags={tags}
+//                 />
+//               )
+//             )}
+//           </ul>
+//         )}
+//       </>
+//     );
+//   }
+// }
